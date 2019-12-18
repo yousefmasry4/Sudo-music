03fe873e17faadece7ddfaebadb40c6164a60178
mohamed osman <mahamedramadan55@yahoo.com>
2019-12-18 16:09:45 +0200

    FLAG//MSH SA7

--- SUDO MUSIC/Form1.cs
+++ SUDO MUSIC/Form1.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -39,11 +42,35 @@ namespace SUDO_MUSIC
             }
             
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        public static IPAddress GetDefaultGateway()
         {
+            return NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up)
+                .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(n => n.GetIPProperties()?.GatewayAddresses)
+                .Select(g => g?.Address)
+                .Where(a => a != null)
+                // .Where(a => a.AddressFamily == AddressFamily.InterNetwork)
+                // .Where(a => Array.FindIndex(a.GetAddressBytes(), b => b != 0) >= 0)
+                .FirstOrDefault();
+        }
 
-
+            private void button1_Click(object sender, EventArgs e)
+        {
+            var ip = GetDefaultGateway().ToString();
+            String[] input = { "192", "168", "1", "100" };
+            String[] strlist = ip.Split('.');
+            int flag = 0;
+            for (int i=0; i<4; i++)
+            {
+                flag+=strlist[i] == input[i] ? 0 : 1;
+                
+            }
+            if(falg != 0)
+            {
+                //msh sa7
+            }
             if (textBox1.Text == "0")
             {
                 this.Hide();
